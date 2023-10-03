a84f4193cfb04e8f0474ac53de90e1f59bd1adcb
simon-smart88 <simon.smart@cantab.net>
2023-10-03 13:40:57 +0100

    start to add app skeleton files

---
+++ inst/app_skeleton/skeleton parameters.R
@@ -0,0 +1,21 @@
+#name of app library
+app_library <- "disagApp"
+
+#whether to include a map
+include_map <- TRUE
+
+#whether to include a table
+include_table <- TRUE
+
+#whether to include a code tab
+include_code <- TRUE
+
+common_objects <- list(
+  ras = NULL,
+  hist = NULL,
+  scat = NULL,
+  meta = NULL,
+  poly = NULL,
+  logger = NULL,
+  state = NULL
+)
