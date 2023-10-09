c898199370a5b7f6312a287ccd8f21ffcaaafb97
simon-smart88 <simon.smart@cantab.net>
2023-10-09 09:37:59 +0100

    skip all shinytest2 on CI

--- tests/testthat/test-plot_scatter.R
+++ tests/testthat/test-plot_scatter.R
@@ -18,6 +18,8 @@ test_that("Check plot_scatter function works as expected", {
 })
 
 test_that("{shinytest2} recording: e2e_plot_scatter", {
+  testthat::skip_on_ci()
+
   app <- shinytest2::AppDriver$new(app_dir = system.file("shiny", package = "SMART"), name = "e2e_plot_scatter")
   app$set_inputs(tabs = "select")
   app$set_inputs(selectSel = "select_user")
