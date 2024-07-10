6bce885e1dda9f2dda58007270e40b9586211b10
simon-smart88 <simon.smart@cantab.net>
2024-07-10 12:46:02 +0100

    add skeleton async module

---
+++ inst/module_skeleton/skeleton_async.R
@@ -0,0 +1,97 @@
+{{id}}_module_ui <- function(id) {
+  ns <- shiny::NS(id)
+  tagList(
+    # UI
+    actionButton(ns("run"), "Run module {{id}}")
+  )
+}
+
+{{id}}_module_server <- function(id, common, parent_session, map) {
+  moduleServer(id, function(input, output, session) {
+
+
+  #create the asynchronous task
+  common$tasks${{id}} <- ExtendedTask$new(function(...) {
+    promises::future_promise({
+      {{id}}(...)
+    })
+  }) |> bslib::bind_task_button("run")
+
+    observeEvent(input$run, {
+      # WARNING ####
+
+      # FUNCTION CALL ####
+      common$logger %>% writeLog("Starting to run {{id}}")
+      # invoke the async task
+      common$tasks${{id}}$invoke()
+      # reactive the results observer if it has already been used
+      results$resume()
+
+      # METADATA ####
+      common$meta${{id}}$setting1 <- input
+    })
+
+
+    results <- observe({
+      # LOAD INTO COMMON ####
+
+      #fetch the result
+      result <- common$tasks${{id}}$result()
+      #suspend the observer
+      results$suspend()
+
+      # check the class of the result is the class when the function runs successfully
+      if (class(result) == "list"){
+
+        common$logger %>% writeLog("{{id}} has completed")
+
+        # TRIGGER
+        gargoyle::trigger("{{id}}")
+
+        # explicitly call the mapping function
+        do.call("{{id}}_module_map", list(map, common))
+        show_map(parent_session)
+
+        # set an input value to use in testing
+        shinyjs::runjs("Shiny.setInputValue('{{id}}-complete', 'complete');")
+      } else {
+        common$logger %>% writeLog(type = "error", result)
+      }
+    })
+
+    output$result <- renderText({
+      gargoyle::watch("{{id}}")
+      # Result
+    })
+
+    return(list(
+      save = function() {
+        # Save any values that should be saved when the current session is saved
+      },
+      load = function(state) {
+        # Load
+      }
+    ))
+  })
+}
+
+{{id}}_module_result <- function(id) {
+  ns <- NS(id)
+
+  # Result UI
+  verbatimTextOutput(ns("result"))
+}
+
+{{id}}_module_map <- function(map, common) {
+  # Map logic
+}
+
+{{id}}_module_rmd <- function(common) {
+  # Variables used in the module's Rmd code
+  list(
+    {{id}}_knit = !is.null(common$some_object),
+    var1 = common$meta${{id}}$setting1,
+    var2 = common$meta${{id}}$setting2
+  )
+}
+
