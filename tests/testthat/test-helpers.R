06eb0764f16c099d4d406c8ca0c6da9a3e3d835d
simon-smart88 <simon.smart@cantab.net>
2024-07-31 16:18:35 +0100

    trim to isolate testing problem

--- tests/testthat/test-helpers.R
+++
@@ -1,5 +0,0 @@
-
-test_that("Check check_url functions as expected", {
-  expect_warning(check_url("https://www.a-very-stupid-and-silly-website.com/"), "https://www.a-very-stupid-and-silly-website.com/ is offline")
-  expect_is(check_url("https://www.google.com"), "httr2_response")
-})
