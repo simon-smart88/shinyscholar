6a8cb503e354cd2d4c20a0ad1b292a183f7cd618
simon-smart88 <simon.smart@cantab.net>
2025-01-23 14:45:21 +0000

    simplify and improve printVecAsis using dput, add tests resolves #24

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -2,43 +2,16 @@
 # MISC #
 ####################### #
 #' @title printVecAsis
-#' @description For internal use. Print vector as character string
-#' @param x vector
-#' @param asChar exclude c notation at the beginning of string
+#' @description For internal use. Print objects as character string
+#' @param x object to print
+#' @return A character string to reproduce the object
 #' @keywords internal
 #' @export
-printVecAsis <- function(x, asChar = FALSE) {
-  if (is.null(x)){
-    return("NULL")
-  }
-  if (is.character(x)) {
-    if (length(x) == 1) {
-      return(paste0("\"", x, "\""))
-    } else {
-      if (asChar == FALSE) {
-        return(paste0("c(", paste(sapply(x, function(a) paste0("\"", a, "\"")),
-                                  collapse = ", "), ")"))
-      } else {
-        return(paste0("(", paste(sapply(x, function(a) paste0("\"", a, "\"")),
-                                 collapse = ", "), ")"))
-      }
-    }
-  } else if ("Date" %in% class(x)){
-    if (length(x) == 1) {
-      return(paste0("as.Date(\"", x ,"\")"))
-    } else {
-      return(paste0("c(", paste(paste0("as.Date(\"", x ,"\")"), collapse = ", "), ")"))
-    }
+printVecAsis <- function(x) {
+  if (is.numeric(x) && length(x) == 1){
+    return(x)
   } else {
-    if (length(x) == 1) {
-      return(x)
-    } else {
-      if (asChar == FALSE) {
-        return(paste0("c(", paste(x, collapse = ", "), ")"))
-      } else {
-        return(paste0("(", paste(x, collapse = ", "), ")"))
-      }
-    }
+    capture.output(dput(x))
   }
 }
 
--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -9,3 +9,15 @@ test_that("Check asyncLog functions as expected", {
   expect_warning(asyncLog(FALSE, "message", type = "warning"), "message")
   expect_equal(asyncLog(TRUE, "message", type = "error"), "message")
 })
+
+test_that("Check printVecAsis function works as expected", {
+  expect_equal(printVecAsis(1) , 1)
+  expect_equal(printVecAsis("a") , "\"a\"")
+  expect_equal(printVecAsis(c(1, 2, 3)) , "c(1, 2, 3)")
+  expect_equal(printVecAsis(c("a", "b", "c")) , "c(\"a\", \"b\", \"c\")")
+  expect_equal(printVecAsis(NULL) , "NULL")
+
+  date_in <- as.Date("2025-01-01")
+  date_out <- eval(parse(text=printVecAsis(date_in)))
+  expect_identical(date_in, date_out)
+})
